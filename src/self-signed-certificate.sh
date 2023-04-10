50c7f912dea1da0f2143f621de7b65f5411ef6d1
arun.alagesan <arun.alagesan@gmail.com>
2023-04-10 14:16:44 +0530

    fixed the wildcard on for generating certificates

--- src/self-signed-certificate.sh
+++ src/self-signed-certificate.sh
@@ -5,6 +5,6 @@ mkdir certs
 
 cd certs
 
-sudo openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config ../config/server.csr.config 
+openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config ../config/server.csr.config 
 
-sudo openssl x509 -req -in server.csr -CA ../ssl/root-CA.pem -CAkey ../ssl/root-CA.key -CAcreateserial -out CASerial.srl -out server.crt -days 500 -sha256 -extfile ../extentions/v3.ext
+openssl x509 -req -in server.csr -CA ../ssl/root-CA.pem -CAkey ../ssl/root-CA.key -CAcreateserial -out CASerial.srl -out server.crt -days 500 -sha256 -extfile ../extentions/v3.ext
